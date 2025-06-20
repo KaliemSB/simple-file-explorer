bc5f15ec16204297da0d93980b74be39352dcab8
KaliemSB <mycael590@gmail.com>
2025-06-20 02:20:40 -0300

    feat: initial sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,82 @@
+import { Download, HardDrive, House, Usb } from "lucide-react";
+import type { FC, PropsWithChildren, ReactNode } from "react";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+
+const fast_access = [
+	{
+		label: "Home",
+		icon: <House />,
+	},
+	{
+		label: "Downloads",
+		icon: <Download />,
+	},
+] satisfies Array<{
+	label: string;
+	icon: ReactNode;
+}>;
+
+const internal_storage = ["KINGSTON SNVS500G", "SAMSUNG 970 EVO Plus 1TB", "CRUCIAL MX500 1TB"];
+
+const external_media = ["SanDisk Cruzer Blade 16GB", "KINGSTON SE9 G2 128GB", "PNY Attache 4 USB 3.0 64GB"];
+
+const SidebarButton: FC<PropsWithChildren & { icon: ReactNode }> = ({ children, icon }) => {
+	return (
+		<Tooltip>
+			<TooltipTrigger asChild>
+				<div>
+					<Button className="w-full justify-start!" size="sm" variant="outline">
+						{icon}
+						<p className="truncate">{children}</p>
+					</Button>
+				</div>
+			</TooltipTrigger>
+			<TooltipContent>
+				<p>{children}</p>
+			</TooltipContent>
+		</Tooltip>
+	);
+};
+
+const SidebarGroup: FC<PropsWithChildren & { label: string }> = ({ children, label }) => {
+	return (
+		<>
+			<p className="text-xs font-medium">{label}</p>
+			{children}
+		</>
+	);
+};
+
+export const AppSidebar: FC = () => {
+	return (
+		<div className="bg-sidebar">
+			<ScrollArea className="h-[calc(100dvh-32px)]">
+				<div className="flex flex-col gap-x-4 gap-y-2 p-4">
+					<SidebarGroup label="Fast Access">
+						{fast_access.map((item) => (
+							<SidebarButton icon={item.icon} key={item.label}>
+								{item.label}
+							</SidebarButton>
+						))}
+					</SidebarGroup>
+					<SidebarGroup label="Internal Storage">
+						{internal_storage.map((item) => (
+							<SidebarButton icon={<HardDrive />} key={item}>
+								{item}
+							</SidebarButton>
+						))}
+					</SidebarGroup>
+					<SidebarGroup label="External Media">
+						{external_media.map((item) => (
+							<SidebarButton icon={<Usb />} key={item}>
+								{item}
+							</SidebarButton>
+						))}
+					</SidebarGroup>
+				</div>
+			</ScrollArea>
+		</div>
+	);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
+import { AppSidebar } from "@/components/app-sidebar";
 import "@/styles/tailwind.css";
 import React, { type FC } from "react";
 import ReactDOM from "react-dom/client";
-import { TitleBar } from "@/components/title-bar";
-import { cn } from "@/lib/utils";
+import { TitleBar } from "./components/title-bar";
 
 const App: FC = () => {
-	const theme = "catppuccin-mocha";
-
 	return (
 		<React.StrictMode>
-			<main className={cn(theme, "h-dvh w-dvw grid grid-cols-[200px_1fr] grid-rows-[32px_1fr]")}>
+			<main className="h-dvh w-dvw grid grid-cols-[200px_1fr] grid-rows-[32px_1fr]">
 				<TitleBar />
-				<div className="bg-sidebar"></div>
+				<AppSidebar />
 				<div className="bg-muted"></div>
 			</main>
 		</React.StrictMode>
